c30d1ef14848630a381aafd8125bdce5a4a8f722
Nikita <yodap321@gmail.com>
2024-10-08 14:43:06 +0300

    feat: Добавлен метод получения начальных параметров

--- src/main/java/backend/academy/Cell.java
+++ src/main/java/backend/academy/Cell.java
@@ -1,5 +1,10 @@
 package backend.academy;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Cell {
     private int x;
     private int y;
--- src/main/java/backend/academy/UserInteraction.java
+++ src/main/java/backend/academy/UserInteraction.java
@@ -16,9 +16,32 @@ public class UserInteraction {
     private Cell start;
     private Cell finish;
 
-    public void getStartParams(InputStream input, OutputStream output) throws IOException{
-        PrintWriter writer = new PrintWriter(new OutputStreamWriter(output),true);
+    public void getStartParams(InputStream input, OutputStream output) throws IOException {
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(output), true);
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
+        writer.println("Введите высоту лабиринта от " + Constants.MIN_SIZE + " до " + Constants.MAX_SIZE);
+        String heightInput = reader.readLine();
+        height = Integer.parseInt(heightInput);
+
+        writer.println("Введите ширину лабиринта от " + Constants.MIN_SIZE + " до " + Constants.MAX_SIZE);
+        String widthInput = reader.readLine();
+        width = Integer.parseInt(widthInput);
+
+        writer.println("Введите позицию начала лабиринта");
+        writer.println("Введите x координату от 0 до " + (height - 1));
+        String xStartInput = reader.readLine();
+        start.setX(Integer.parseInt(xStartInput));
+        writer.println("Введите y координату от 0 до " + (width - 1));
+        String yStartInput = reader.readLine();
+        start.setY(Integer.parseInt(yStartInput));
+
+        writer.println("Введите позицию конца лабиринта");
+        writer.println("Введите x координату от 0 до " + (height - 1));
+        String xFinishInput = reader.readLine();
+        start.setX(Integer.parseInt(xFinishInput));
+        writer.println("Введите y координату от 0 до " + (width - 1));
+        String yFinishInput = reader.readLine();
+        start.setY(Integer.parseInt(yFinishInput));
     }
 }
